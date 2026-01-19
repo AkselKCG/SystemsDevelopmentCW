8309ce3427adb5ad4ed795a78c1b4da9e69b33cb
Aksel Kesici (s5620274) <s5620274@bournemouth.ac.uk>
2026-01-19 16:44:23 +0000

    Render dashboard template and add basic styling

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
-@app.get("/")
+@app.route("/")
 def home():
-    return "Clinic Booking System is running"
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
