0e09699f115be7e4c12939ccfaa3c3d7fdcbd063
Aksel Kesici (s5620274) <s5620274@bournemouth.ac.uk>
2026-01-19 17:24:10 +0000

    Add navigation and appointments and services pages

--- src/main.py
+++ src/main.py
@@ -6,5 +6,13 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 def home():
     return render_template("dashboard.html")
 
+@app.route("/appointments")
+def appointments():
+    return render_template("appointments.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
